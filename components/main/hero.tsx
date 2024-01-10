0fe0f7222c75092e1509d2d8ffd5115cd1adedbc
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-10 13:41:38 +0000

    hero section implemented

---
+++ components/main/hero.tsx
@@ -0,0 +1,18 @@
+import { HeroContent } from "@/components/sub/hero-content";
+
+export const Hero = () => {
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+
+      <HeroContent />
+    </div>
+  );
+};
---
+++ components/sub/hero-content.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/lib/motion";
+import Image from "next/image";
+
+export const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Fullstack Developer Portfolio
+          </h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              the best
+            </span>{" "}
+            project experience.
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a Full Stack Software Engineer with experience in Website,
+          Mobile, and Software development. Check out my projects and skills.
+        </motion.p>
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn more
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+          draggable={false}
+          className="select-none"
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
