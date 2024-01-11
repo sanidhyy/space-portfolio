586d9dca4a23fb57001584f5c24e57db49b08de6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-11 16:58:23 +0000

    skills section implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/main/hero";
+import { Skills } from "@/components/main/skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
+        <Skills />
       </div>
     </main>
   );
---
+++ components/main/skills.tsx
@@ -0,0 +1,25 @@
+import { FRONTEND_SKILL } from "@/constants";
+import { SkillDataProvider } from "@/components/sub/skill-data-provider";
+
+export const Skills = () => {
+  return (
+    <section
+      id="skills"
+      style={{ transform: "scale(0.9)" }}
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
+    >
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {FRONTEND_SKILL.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
