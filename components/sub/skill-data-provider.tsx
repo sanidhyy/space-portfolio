600548f17064509119fe7142e1b4679fbcf6dcf6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-11 16:58:50 +0000

    image alt fix

--- components/sub/skill-data-provider.tsx
+++ components/sub/skill-data-provider.tsx
@@ -39,7 +39,7 @@ export const SkillDataProvider = ({
       custom={index}
       transition={{ delay: index * animationDelay }}
     >
-      <Image src={src} width={width} height={height} alt={`Skill ${index}`} />
+      <Image src={src} width={width} height={height} alt={name} />
     </motion.div>
   );
 };
