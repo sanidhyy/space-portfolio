d8bed1ce5d68bcf3b22c638af0305ed95f612e3a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-11 16:57:43 +0000

    skills data provider implemented

---
+++ components/sub/skill-data-provider.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+
+type SkillDataProviderProps = {
+  src: string;
+  name: string;
+  width: number;
+  height: number;
+  index: number;
+};
+
+export const SkillDataProvider = ({
+  src,
+  name,
+  width,
+  height,
+  index,
+}: SkillDataProviderProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const imageVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  const animationDelay = 0.3;
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      variants={imageVariants}
+      animate={inView ? "visible" : "hidden"}
+      custom={index}
+      transition={{ delay: index * animationDelay }}
+    >
+      <Image src={src} width={width} height={height} alt={`Skill ${index}`} />
+    </motion.div>
+  );
+};
