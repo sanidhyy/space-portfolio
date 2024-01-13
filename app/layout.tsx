4028b2e4a94179841188379b7e46fc2a71ecbc17
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-13 16:41:53 +0000

    site config added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
 
 import { Footer } from "@/components/main/footer";
 import { Navbar } from "@/components/main/navbar";
 import { StarsCanvas } from "@/components/main/star-background";
+import { siteConfig } from "@/config";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "John Doe | Space Portfolio",
-  description: "Welcome to my space portfolio.",
+export const viewport: Viewport = {
+  themeColor: "#030014",
 };
+
+export const metadata: Metadata = siteConfig;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
