1177b61dee60cd9ee04f3cb78e027cc51d466a28
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-13 15:15:54 +0000

    fix project link

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -3,7 +3,10 @@ import { PROJECTS } from "@/constants";
 
 export const Projects = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-20">
+    <section
+      id="projects"
+      className="flex flex-col items-center justify-center py-20"
+    >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
@@ -18,6 +21,6 @@ export const Projects = () => {
           />
         ))}
       </div>
-    </div>
+    </section>
   );
 };
