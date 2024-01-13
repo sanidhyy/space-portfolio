c0c989cfab6f91641242a32895d22c30ce49e214
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-13 13:40:22 +0000

    projects updated

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -1,4 +1,5 @@
 import { ProjectCard } from "@/components/sub/project-card";
+import { PROJECTS } from "@/constants";
 
 export const Projects = () => {
   return (
@@ -7,21 +8,14 @@ export const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+        {PROJECTS.map((project) => (
+          <ProjectCard
+            key={project.title}
+            src={project.image}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </div>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -247,3 +247,27 @@ export const OTHER_SKILL = [
     height: 60,
   },
 ] as const;
+
+export const PROJECTS = [
+  {
+    title: "Modern Next.js 14 Portfolio",
+    description:
+      'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
+    image: "/project-1.png",
+    link: "",
+  },
+  {
+    title: "Interactive Cards Portfolio",
+    description:
+      'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
+    image: "/project-2.png",
+    link: "",
+  },
+  {
+    title: "Space Themed Website",
+    description:
+      'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
+    image: "/project-3.png",
+    link: "",
+  },
+] as const;
